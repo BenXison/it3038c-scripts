fea1886f9049145ef6a05d57214edae7f158096c
BenXison <142823746+BenXison@users.noreply.github.com>
2023-10-01 14:52:49 -0400

    Add files via upload

--- labs/Lab5.py
+++ labs/Lab5.py
@@ -1,9 +1,40 @@
-print(‘What is your name?’) 
+import random
+import math
 
-myName = input() 
+lower = int(input("Enter beginning of number range:- "))
 
-print(‘Hello, ‘ + myName + ‘. That is a good name. How old are you?’) 
 
-myAge = input() 
+upper = int(input("Enter end of number range:- "))
 
-print(myAge+’? That’s funny, I'm only a few seconds old.’) 
+
+x = random.randint(lower, upper)
+print("\n\tYou've only ",
+	round(math.log(upper - lower + 1,2)),
+	" chances to guess the integer!\n")
+
+
+count = 0
+
+while count < math.log(upper - lower + 1, 2):
+	count += 1
+
+	
+	guess = int(input("Guess a number:- "))
+
+	
+	if x == guess:
+		print("Congratulations! You did it in ",
+			count, " tries!")
+		
+		break
+	elif x > guess:
+		print("You guessed too low!")
+	elif x < guess:
+		print("You Guessed too high!")
+
+
+if count >= math.log(upper - lower + 1, 2):
+	print("\nThe number is %d" % x)
+	print("\tBetter Luck Next time!")
+
+
